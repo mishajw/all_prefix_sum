4aacff83868785cee04a77ba1fd59115d4232a85
Misha Wagner <mishajw@gmail.com>
2018-02-16 13:14:05 +0000

    Add broken blelloch scan function

--- src/main.cu
+++ src/main.cu
@@ -12,16 +12,85 @@
 
 #define GLOBAL_INDEX blockIdx.x * blockDim.x + threadIdx.x
 
+#define BLOCK_SIZE 1024
+
 // The size of the array to test on
 static const size_t ARRAY_SIZE = 1000;
 
 typedef int32_t num_t;
 
+// Performs exclusive Blelloch scan on a block level
+__global__
+void blelloch_block_scan(const num_t *g_input, num_t *g_output, size_t length) {
+  __shared__ num_t s_temp[BLOCK_SIZE * 2];
+  size_t global_index = GLOBAL_INDEX;
+  size_t index = threadIdx.x;
+
+  // Copy global memory into shared
+  if (global_index < length) {
+    s_temp[index] = g_input[global_index];
+  }
+
+  // Up sweep
+  for (size_t stride = 1; stride < length; stride *= 2) {
+    __syncthreads();
+    if ((index + 1) % (stride * 2) == 0) {
+      s_temp[index] += s_temp[index - stride];
+    }
+  }
+
+  // Reset last element
+  if (index == 0) {
+    s_temp[BLOCK_SIZE - 1] = 0;
+  }
+
+  // Down sweep
+  for (size_t stride = BLOCK_SIZE / 2; stride > 0; stride /= 2) {
+    __syncthreads();
+    if ((index + 1) % (stride * 2) == 0) {
+      num_t old_index_value = s_temp[index];
+      s_temp[index] += s_temp[index - stride];
+      s_temp[index - stride] = old_index_value;
+    }
+  }
+
+  // Copy results into global memory
+  if (global_index < length) {
+    g_output[global_index] = s_temp[index];
+  }
+}
+
 // Performs all prefix sum on `input` and stores the result in `output` in
 // parallel on a GPU
 // Assumes both `input` and `output` are allocated with size `length`
 void scan(const num_t *input, num_t *output, size_t length) {
-  // TODO: Implement
+  size_t array_size = sizeof(num_t) * length;
+
+  // Set up input on device
+  num_t *g_input = NULL;
+  CUDA_ERROR(
+      cudaMalloc((void **)&g_input, array_size),
+      "Couldn't allocate memory for input on device");
+  CUDA_ERROR(
+      cudaMemcpy(g_input, input, array_size, cudaMemcpyHostToDevice),
+      "Couldn't copy input to device");
+
+  // Setup output on device
+  num_t *g_output = NULL;
+  CUDA_ERROR(
+      cudaMalloc((void **)&g_output, array_size),
+      "Couldn't allocate memory for output on device");
+
+  if (length < BLOCK_SIZE) {
+    blelloch_block_scan<<<1, BLOCK_SIZE>>>(g_input, g_output, length);
+  } else {
+    // TODO: Implement
+  }
+
+  // Copy results to host
+  CUDA_ERROR(
+      cudaMemcpy(output, g_output, array_size, cudaMemcpyDeviceToHost),
+      "Couldn't copy output to host");
 }
 
 // Performs all prefix sum on `input` and stores the result in `output`
